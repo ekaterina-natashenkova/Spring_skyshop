4684e64b153a679c1624b1bac287f99cf57bb2b2
EKATERINA NATASHENKOVA <vasilieva-ekaterina@yandex.ru>
2025-07-21 23:41:09 +0300

    homework done-2

--- skyshop/src/test/java/org/skypro/skyshop/service/BasketServiceTest.java
+++ skyshop/src/test/java/org/skypro/skyshop/service/BasketServiceTest.java
@@ -31,6 +31,7 @@ class BasketServiceTest {
 
     @Mock
     private StorageService storageService;
+    
     @Mock
     private ProductBasket productBasket;
 
@@ -51,13 +52,10 @@ class BasketServiceTest {
         Searchable addingProduct = new SimpleProduct("apple", 120, UUID.randomUUID());
         when(storageService.getProductById(addingProduct.getId())).thenReturn(Optional.empty());
 
-        // test
         productBasket.addProductsBasket(UUID.randomUUID());
         assertThrows(NoSuchProductException.class, () -> basketService.addProductBasket(addingProduct.getId()));
 
-        // check
         verify(productBasket, never()).addProductsBasket(addingProduct.getId());
-
     }
 
     @Test
@@ -66,25 +64,21 @@ class BasketServiceTest {
         Product addingProduct2 = new SimpleProduct("чай", 120, UUID.randomUUID());
         when(storageService.getProductById(addingProduct2.getId())).thenReturn(Optional.of(addingProduct2));
 
-        // test
-        productBasket.addProductsBasket(addingProduct2.getId());
+        basketService.addProductBasket(addingProduct2.getId());
 
-        // check
         verify(productBasket, only()).addProductsBasket(addingProduct2.getId());
-
     }
 
     @Test
     void getUserBasket_WhenProductBasketIsEmpty_ThenReturnIsEmptyBasket() {
 
         when(productBasket.getProductsBasket()).thenReturn(Collections.emptyMap());
+        UserBasket userBasket = basketService.getUserBasket();
 
-        // test
-        assertNotNull(productBasket.getProductsBasket());
-        assertTrue(productBasket.getProductsBasket().isEmpty());
-        assertEquals(0, productBasket.getProductsBasket());
+        assertNotNull(userBasket);
+        assertTrue(userBasket.getBasketItems().isEmpty());
+        assertEquals(0, userBasket.getTotal());
 
-        // check
         verify(productBasket, only()).getProductsBasket();
         verifyNoInteractions(storageService);
     }
@@ -93,42 +87,14 @@ class BasketServiceTest {
     void getUserBasket_WhenProductBasketExists_ThenReturnExistsBasket() throws IllegalAccessException {
 
         Product addingProduct3 = new SimpleProduct("кофе", 120, UUID.randomUUID());
-        UserBasket basketItemsList = new UserBasket(List.of(new BasketItem(addingProduct3,1)));
+        UserBasket userBasket = new UserBasket(List.of(new BasketItem(addingProduct3,1)));
+        when(productBasket.getProductsBasket()).thenReturn(Map.of(addingProduct3.getId(), 1));
         when(storageService.getProductById(addingProduct3.getId())).thenReturn(Optional.of(addingProduct3));
-        when(productBasket.getProductsBasket()).thenReturn((Map<UUID, Integer>) basketItemsList);
 
-        // test
-        assertEquals(basketService.getUserBasket(),basketItemsList);
+        assertEquals(userBasket.getBasketItems(),userBasket);
 
-        // check
         verify(productBasket, only()).getProductsBasket();
         verify(storageService, only()).getProductById(addingProduct3.getId());
     }
-
-
-
-
-    //   //given
-    //        final int quantity = 3;
-    //        final int pricePerProduct = 100;
-    //        final int expectedTotal = quantity * pricePerProduct;
-    //        Map<UUID, Integer> itemsMap = Collections.singletonMap(existingProductId, quantity);
-    //
-    //        //when
-    //        when(productBasket.getItems()).thenReturn(itemsMap);
-    //        when(storageService.getProductById(existingProductId)).thenReturn(Optional.of(existingProduct));
-    //        when(existingProduct.getPrice()).thenReturn(pricePerProduct);
-    //
-    //        UserBasket userBasket = basketService.getUserBasket();
-    //
-    //        //then
-    //        assertNotNull(userBasket);
-    //        assertEquals(1, userBasket.getItems().size());
-    //        assertEquals(expectedTotal, userBasket.getTotal());
-    //
-    //        BasketItem basketItem = userBasket.getItems().get(0);
-    //        assertEquals(existingProduct, basketItem.getProduct());
-    //        assertEquals(quantity, basketItem.getQuantity());
-    //
 
 }
